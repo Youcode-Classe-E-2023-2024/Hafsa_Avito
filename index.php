c4de08b9b76d12f0641576d4f7eb1c93ab71b163
HAFSA159 <hafsaelmoatassim99@gmail.com>
2023-11-15 17:44:44 +0100

    add page

--- index.php
+++ index.php
@@ -4,63 +4,87 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
-    <title>Document</title>
+    <title>Annonce</title>
+    
     <style>
+        body {
+          background-color: #f8f9fa;
+        }
 
-    body {
-      font-family: 'Arial', sans-serif;
-      background-color: #f8f9fa;
-    }
+        .container {
+          max-width: 900px;
+          margin: 50px auto;
+          background-color: #ffffff;
+          padding: 20px;
+          border-radius: 8px;
+          box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+        }
 
-    form {
-      max-width: 600px;
-      margin: 50px auto;
-      background-color: #ffffff;
-      padding: 20px;
-      border-radius: 8px;
-      box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
-    }
+        h2 {
+          text-align: center;
+          padding: 1em;
+        }
 
-    label {
-      font-weight: bold;
-    }
+        label {
+          font-weight: bold;
+        }
 
-    .form-check-label {
-      font-weight: normal;
-    }
+        .btn-primary {
+          background-color: black;
+          width: 100%;
+        }
 
-    .btn-primary {
-      background-color: #007bff;
-      border-color: #007bff;
-    }
-
-    .btn-primary:hover {
-      background-color: #0056b3;
-      border-color: #0056b3;
-    }
-    </style>
+        .btn-primary:hover {
+          background-color: orangered;
+        }
+  </style>
 
 </head>
 <body>
     
-<form>
-  <div class="mb-3">
-    <label for="exampleInputEmail1" class="form-label">Email address</label>
-    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
-    <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
-  </div>
-  <div class="mb-3">
-    <label for="exampleInputPassword1" class="form-label">Password</label>
-    <input type="password" class="form-control" id="exampleInputPassword1">
-  </div>
-  <div class="mb-3 form-check">
-    <input type="checkbox" class="form-check-input" id="exampleCheck1">
-    <label class="form-check-label" for="exampleCheck1">Check me out</label>
-  </div>
-  <button type="submit" class="btn btn-primary">Submit</button>
-</form>
+<div class="container mt-5">
+    <h2>Create an Announcement</h2>
 
+    <form action="add.php"> 
+      <div class="mb-3">
+        <label for="firstname" class="form-label">Firstname</label>
+        <input type="text" class="form-control" id="Firstname" required>
+      </div>
+      <div class="mb-3">
+        <label for="lastname" class="form-label">Lastname</label>
+        <input type="text" class="form-control" id="Lastname" required>
+      </div>
+      <div class="mb-3">
+        <label for="email" class="form-label">Email</label>
+        <input class="form-control" id="email" type="email" required></input>
+      </div>
+      <div class="mb-3">
+        <label for="pwd" class="form-label">Password</label>
+        <input class="form-control" id="pwd" type="password" required></input>
+      </div>
+      <div class="mb-3">
+        <label for="category" class="form-label">Category</label>
+        <select class="form-select" id="category" required>
+          <option value="" disabled selected>Select a category</option>
+          <option value="announcement">Electronique</option>
+          <option value="event">Logement</option>
+          <option value="sale">Voiture</option>
+        </select>
+      </div>
+      <button type="submit" class="btn btn-primary">Submit</button>
+    </form>
 
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>    
+</div>
+
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>    
 </body>
 </html>
+
+<!-- CREATE TABLE IF NOT EXISTS Users (
+    UserID INT AUTO_INCREMENT PRIMARY KEY,
+    FirstName VARCHAR(50) NOT NULL,
+    LastName VARCHAR(50) NOT NULL,
+    Email VARCHAR(100) UNIQUE NOT NULL,
+    Password VARCHAR(255) NOT NULL,
+    CategoryOfAnnouncement VARCHAR(50) NOT NULL
+); -->
